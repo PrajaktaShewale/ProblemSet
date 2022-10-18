99dda84f6184c60dd9bc685b8f92da8cdc3f271d
PrajaktaShewale <shewalep94@gmail.com>
2022-10-18 23:49:18 +0530

    First commit

---
+++ Demo1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; // Import's
+
+namespace Example //Name space is nthg but the project name
+{
+    class Program //blue print of object using which i can create multiple objects,
+                  //It containts states ( variable initializations ) and behaviour ( Methods ) of class
+    {
+        public static void Main(string[] args) //Static Method
+        {
+
+        }
+    }
+}
